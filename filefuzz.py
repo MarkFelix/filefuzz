4b20cc880c3a2222772d396c5f8b02ea3c04f535
Simon_Phoenix <mr.president.80@hotmail.com>
2017-12-30 10:20:22 +1100

    Winamp 5.12 file fuzzer
    Winamp 5.12 file fuzzer

--- filefuzz.py
+++ filefuzz.py
@@ -1,7 +1,8 @@
 #############################################################################################
 # Fuzzer based on Primal Security's FTP Server fuzzer.										#
-# http://www.primalsecurity.net/0x0-exploit-tutorial-buffer-overflow-vanilla-eip-overwrite-2#                                      #
+# http://www.primalsecurity.net/0x0-exploit-tutorial-buffer-overflow-vanilla-eip-overwrite-2#
 # This fuzzer is made for fuzzing WinAmp .pls playlist files.                               #
+# Original winamp 5.12 exploit: https://www.exploit-db.com/exploits/3422/					#
 # Spews out a bunch of fuzz files starting at a buffer size of 100 A's and goes endlessly.  #
 # Can be adapted to any type of file, just need to change the design of the template.       #
 #############################################################################################
